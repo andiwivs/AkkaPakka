d9a358dea493f107f412c82e1fcdb3290641c00d
Andrew Withers <andreww@thomasdev.co.uk>
2018-03-15 22:01:46 +0000

    Add custom supervisor strategy example

--- src/AkkaPakka/Actors/PlaybackStatisticsActor.cs
+++ src/AkkaPakka/Actors/PlaybackStatisticsActor.cs
@@ -1,6 +1,7 @@
 ﻿using Akka.Actor;
-using System;
 using Akka.DI.Core;
+using System;
+using AkkaPakka.Exceptions;
 
 namespace AkkaPakka.Actors
 {
@@ -17,6 +18,32 @@ namespace AkkaPakka.Actors
             var moviePlayCounterActorProps = Context.DI().Props<MoviePlayCounterActor>();
 
             Context.ActorOf(moviePlayCounterActorProps, "MoviePlayCounter");
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            var customStrategy = new OneForOneStrategy(exception =>
+            {
+                // for (simulated) currupt state, we need to force a restart to correct the state issue...
+                if (exception is SimulatedCorruptStateException)
+                {
+                    _logger.WriteDebug("Supervisor strategy: Restart as (simulated) corrupt state detected");
+                    return Directive.Restart;
+                }
+
+                // for a less significant issue, we can allow the actor to resume...
+                if (exception is SimulatedTerribleMovieException)
+                {
+                    _logger.WriteDebug("Supervisor strategy: Resume as (simulated) low importance issue detected");
+                    return Directive.Resume;
+                }
+
+                // otherwise, revert to default behaviour (ie to restart the actor)
+                _logger.WriteDebug("Supervisor strategy: Restart as fallback behaviour");
+                return Directive.Restart;
+            });
+
+            return customStrategy;
         }
 
         #region lifecycle hooks
